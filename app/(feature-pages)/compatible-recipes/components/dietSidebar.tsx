eeac75a4125f2da90e7422ce9e047924adff49d3
Muhaiminul Islam Ninad <ninadgns@gmail.com>
2025-03-06 00:24:31 +0600

    allergy in sidebar

--- app/(feature-pages)/compatible-recipes/components/dietSidebar.tsx
+++ app/(feature-pages)/compatible-recipes/components/dietSidebar.tsx
@@ -2,12 +2,14 @@
 import { Button } from "@/components/ui/button"
 import { SidebarInset } from "@/components/ui/sidebar"
 import { Diets } from "@/utils/type"
+import { userAllergy } from "../page"
 
 type DietSidebarProps = {
     diets: Diets[]
+    allergies: userAllergy[]
 }
 
-export function DietSidebar({ diets }: DietSidebarProps) {
+export function DietSidebar({ diets, allergies }: DietSidebarProps) {
     return (
         <SidebarInset>
             <div className="p-4 max-w-80 border-r h-full">
@@ -23,6 +25,21 @@ export function DietSidebar({ diets }: DietSidebarProps) {
                     </ul>
                 ) : (
                     <p className="text-sm text-gray-500">No diet preferences found.</p>
+                )}
+
+
+                <h2 className="text-lg font-semibold mt-4">Allergies</h2>
+                {allergies.length > 0 ? (
+                    <ul className="space-y-2">
+                        {allergies.map((allergy) => (
+                            <li key={allergy.ingredient.ingredient_id} className="p-2 border rounded">
+                                {allergy.ingredient.name}
+                                <p className="text-sm">{allergy.severity}</p>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p className="text-sm text-gray-500">No allergies found.</p>
                 )}
 
                 <Button
