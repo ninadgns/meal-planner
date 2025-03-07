b8844eae24c361a659a4feeab1deed104b5c203e
Muhaiminul Islam Ninad <ninadgns@gmail.com>
2025-03-07 23:04:17 +0600

    Update chart.tsx

--- app/(feature-pages)/admin/components/chart.tsx
+++ app/(feature-pages)/admin/components/chart.tsx
@@ -19,31 +19,34 @@ const chartConfig = {
 } satisfies ChartConfig
 
 export function UserToDietChart({ chartData }: { chartData: DietUser[] }) {
-    return (
-        <ChartContainer config={chartConfig} className="aspect-auto h-[250px] w-4/5 mx-auto">
-            <BarChart accessibilityLayer data={chartData}>
 
-                <ChartTooltip
-                    content={
-                        <ChartTooltipContent
-                            className="w-[150px]"
-                            nameKey="views"
-                        />
-                    }
-                />
-                <CartesianGrid vertical={false} />
-                <Bar dataKey="follower_count"  barSize={60} fill="hsl(var(--chart-2)" radius={3} />
-                <XAxis
-                    dataKey="diet_name"
-                    tickLine={false}
-                    axisLine={false}
-                    tickMargin={8}
-                    minTickGap={10}
-                    tickFormatter={(value) => {
-                        return value;
-                    }}
-                />
-            </BarChart>
-        </ChartContainer>
+    return (
+        <div className="">
+            <h2 className="text-xl font-bold mb-4">User to Diet Chart</h2>
+            <ChartContainer config={chartConfig} className="aspect-auto h-[250px] w-4/5 mx-auto">
+                <BarChart accessibilityLayer data={chartData}>
+                    <ChartTooltip
+                        content={
+                            <ChartTooltipContent
+                                className="w-[150px]"
+                                nameKey="views"
+                            />
+                        }
+                    />
+                    <CartesianGrid vertical={false} />
+                    <Bar dataKey="follower_count"  barSize={60} fill="hsl(var(--chart-2)" radius={3} />
+                    <XAxis
+                        dataKey="diet_name"
+                        tickLine={false}
+                        axisLine={false}
+                        tickMargin={8}
+                        minTickGap={10}
+                        tickFormatter={(value) => {
+                            return value;
+                        }}
+                    />
+                </BarChart>
+            </ChartContainer>
+        </div>
     )
 }
